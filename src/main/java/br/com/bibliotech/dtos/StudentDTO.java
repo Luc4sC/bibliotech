104796a83cf716e1ef13b4821ac50c6ecef7ad69
Luc4sC <lucasoull@gmail.com>
2024-09-29 21:23:56 -0300

    Revisando DTOs

--- src/main/java/br/com/bibliotech/dtos/StudentDTO.java
+++ src/main/java/br/com/bibliotech/dtos/StudentDTO.java
@@ -1,8 +1,12 @@
 package br.com.bibliotech.dtos;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
+import java.time.LocalDate;
+
 public record StudentDTO(@NotEmpty @Size(min = 3, max = 10) String rm, @NotEmpty @Size(min = 5, max = 50) String fullName,
-                         @NotEmpty @Size(min = 11, max = 11) String cellPhone, @NotEmpty AddressDTO addressDTO) {
+                         @NotEmpty @Past LocalDate birthdate, @NotEmpty @Size(min = 11, max = 11) String cellPhone,
+                         @NotEmpty AddressDTO addressDTO) {
 }
